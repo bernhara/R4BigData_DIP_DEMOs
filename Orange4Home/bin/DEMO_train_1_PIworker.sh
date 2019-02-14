608474e4dfe55041edcb748882572dc57a7c62b0
BERNHARD Raphaël <raphael.bernhard@orange.com>
2019-02-14 10:09:29 +0100

    check all args from standalone run

--- Orange4Home/bin/DEMO_train_1_PIworker.sh
+++ Orange4Home/bin/DEMO_train_1_PIworker.sh
@@ -15,24 +15,28 @@ if [ -z "${MLR_TRAINING_ARGs}" ]
 then
     MLR_TRAINING_ARGs=''
 
-    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --train_file=/home/dip/datasets/o4h_location_labels_classify.libsvm.train.txt"
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --train_file=/home/dip/datasets/zz.txt"
+
     MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --use_weight_file=false"
     MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --weight_file="
+
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --num_train_eval=20"
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --num_test_eval=20"
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --perform_test=false"
+
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --num_epochs=40"
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --num_batches_per_eval=10"
+
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --lambda=0"
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --lr_decay_rate=0.99"
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --num_train_eval=10000"
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --init_lr=0.01"
 
     MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --num_comm_channels_per_client=1"
     MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --staleness=2"
     MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --num_app_threads=3"
-    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --num_batches_per_epoch=10 --num_epochs=40"
-    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --output_file_prefix="${output_prefix_file}""
-    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --lr_decay_rate=0.99 --num_train_eval=10000"
-    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --global_data=${mlr_arg_global_data}"
-    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --init_lr=0.01"
-    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --num_test_eval=20 --perform_test=false --num_batches_per_eval=10 --lambda=0"
-    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --hostfile=${tmp_dir}/localserver"
-    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --train_file=${train_file}"
 
-    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --client_id="${this_worker_index}""
-    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --num_clients=${nb_workers}"
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --output_file_prefix=/tmp/ZZ_"
 fi
 
 TRAINING_TUNING_ARGs="${MLR_TRAINING_ARGs}" \
