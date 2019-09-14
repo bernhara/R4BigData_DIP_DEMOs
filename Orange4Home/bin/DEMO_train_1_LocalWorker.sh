05fa050985a3f2dd62cf4adfbb427c0e2b752592
BERNHARD Raphaël <raphael.bernhard@orange.com>
2019-09-14 19:29:50 +0200

    added missed file (1 local worker) Is it working??

---
+++ Orange4Home/bin/DEMO_train_1_LocalWorker.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+
+HERE=$( dirname $0 )
+CMD=$( basename $0 )
+
+DEMO_ROOT_DIR="${HERE}/.."
+
+: ${REMOTE_DATASET_DIR:=$( readlink -f "${DEMO_ROOT_DIR}/out" )}
+
+: ${tmp_dir:=`mktemp -u -p "${DEMO_ROOT_DIR}/tmp"`}
+# make this default for all child scripts
+export tmp_dir
+
+# limit training time to 5mn
+: ${WORKER_ENV_TRAINING_TIMEOUT=5}
+: ${WORKER_ENV_VERBOSE=1}
+
+
+: ${worker_hostname_01:="localhost"}
+: ${worker_remote_user_01:="orba6563"}
+
+if [ -z "${MLR_TRAINING_ARGs}" ]
+then
+    MLR_TRAINING_ARGs=''
+
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --train_file=/home/dip/datasets/o4h_location_labels_classify.libsvm.train.txt"
+
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --use_weight_file=false"
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --weight_file="
+
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --num_train_eval=20"
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --num_test_eval=20"
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --perform_test=false"
+
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --num_epochs=40"
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --num_batches_per_eval=10"
+
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --lambda=0"
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --lr_decay_rate=0.99"
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --num_train_eval=10000"
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --init_lr=0.01"
+
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --num_comm_channels_per_client=1"
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --staleness=2"
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --num_app_threads=3"
+
+    MLR_TRAINING_ARGs="${MLR_TRAINING_ARGs} --output_file_prefix=/tmp/ZZ_"
+fi
+
+TRAINING_ARGs="${MLR_TRAINING_ARGs}" \
+WORKER_ENV_TRAINING_TIMEOUT="${WORKER_ENV_TRAINING_TIMEOUT}" \
+WORKER_ENV_VERBOSE="${WORKER_ENV_VERBOSE=1}" \
+"${DEMO_ROOT_DIR}/../Utils/trainAllPetuumMlrWorkers.sh" \
+${worker_remote_user_01}@${worker_hostname_01}:${REMOTE_DATASET_DIR}
