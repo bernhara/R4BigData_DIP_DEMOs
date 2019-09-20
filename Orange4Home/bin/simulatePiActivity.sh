1b5075ffc8f6b09fdea760d4ae88085e60a67ca5
BERNHARD Raphaël <raphael.bernhard@orange.com>
2019-09-20 14:42:18 +0200

    not under RCS!!!

--- Orange4Home/bin/simulatePiActivity.sh
+++ Orange4Home/bin/simulatePiActivity.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 
-# $Id: $
-
-
 remote_command="timeout 3s /bin/bash -c 'while true; do touch /tmp/nothing; t=$( shuf -i1-9 -n1 ); sleep 0.0$t; echo \$\$; done'"
 for i in 01 02 03 04 05 06 07 08
 do
