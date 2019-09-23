43e7f07c5544d1ec5c31832a9670055cd2cb3dd0
BERNHARD Raphaël <raphael.bernhard@orange.com>
2019-09-23 09:30:15 +0200

    mini = 1s.

--- Orange4Home/bin/simulatePiActivity.sh
+++ Orange4Home/bin/simulatePiActivity.sh
@@ -2,9 +2,9 @@
 
 : ${SIMULATION_DELAY:=30s}
 
-remote_command="set -vx; hostname; timeout ${SIMULATION_DELAY} /bin/bash -c 'mkdir -p /persist_store/DEMOs/tmp; while true; do cp -r /persist_store/DEMOs/Orange4Home/datasets/ /persist_store/DEMOs/tmp/; sync; t=$( shuf -i1-9 -n1 ); sleep 0.$t; echo \$\$; hostname; done'"
+remote_command="set -vx; hostname; timeout ${SIMULATION_DELAY} /bin/bash -c 'mkdir -p /persist_store/DEMOs/tmp; while true; do cp -r /persist_store/DEMOs/Orange4Home/datasets/ /persist_store/DEMOs/tmp/; sync; t=$( shuf -i1-9 -n1 ); sleep 1.$t; echo \$\$; hostname; done'"
 for i in 01 02 03 04 05 06 07 08
 do
-    ssh dip@s-pituum-$i "${remote_command}"
+    ssh -f dip@s-pituum-$i "${remote_command}"
 done
 
